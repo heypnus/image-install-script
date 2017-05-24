adb57c5b2a7dfb678465f79e1ddc02e8acf75675
heypnus <heypnus@gmail.com>
2017-05-24 17:08:37 +0800

    change official yum source to 163

--- RedHat/RedHat_6.4/install.sh
+++ RedHat/RedHat_6.4/install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# change official yum source to 163
+rpm -qa | grep yum | xargs rpm -e --nodeps
+wget http://mirrors.163.com/centos/6/os/x86_64/Packages/python-iniparse-0.3.1-2.1.el6.noarch.rpm
+wget http://mirrors.163.com/centos/6/os/x86_64/Packages/yum-metadata-parser-1.1.2-16.el6.x86_64.rpm
+wget http://mirrors.163.com/centos/6/os/x86_64/Packages/yum-3.2.29-81.el6.centos.noarch.rpm
+wget http://mirrors.163.com/centos/6/os/x86_64/Packages/yum-plugin-fastestmirror-1.1.30-40.el6.noarch.rpm
+rpm -ivh python-iniparse-0.3.1-2.1.el6.noarch.rpm
+rpm -ivh yum-metadata-parser-1.1.2-16.el6.x86_64.rpm
+rpm -e --nodeps python-urlgrabber-3.9.1-8.el6.noarch
+wget http://mirrors.163.com/centos/6/os/x86_64/Packages/python-urlgrabber-3.9.1-11.el6.noarch.rpm
+rpm -ivh yum-3.2.29-81.el6.centos.noarch.rpm yum-plugin-fastestmirror-1.1.30-40.el6.noarch.rpm python-urlgrabber-3.9.1-11.el6.noarch.rpm
+wget http://mirrors.163.com/.help/CentOS6-Base-163.repo
+mv CentOS6-Base-163.repo /etc/yum.repos.d/
+sed -i 's/\$releasever/6/g'  /etc/yum.repos.d/CentOS6-Base-163.repo
+yum clean all
+yum makecache
+rm *.rpm
+
 yum -y update python
 yum -y install openssh-server
 chkconfig sshd on
